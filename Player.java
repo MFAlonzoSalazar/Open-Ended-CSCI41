47200933a0002b77e1fabcd4a858ac8b949db1b9
DylanCordoza <dylan.cordoza@gmail.com>
2018-05-07 13:05:51 -0700

    Update and rename playerClass.java to Player.java
    Updated the code to work better with other files.

---
+++ Player.java
@@ -0,0 +1,27 @@
+public class Player
+{
+	private String player = "";
+	private int score = 0;
+	private Hand playerHand
+
+	public final String getPlayer()
+	{
+		return player;
+	}
+
+	public final void setplayer(String newPlayer)
+	{
+		player = newPlayer;
+	}
+
+	public final int getScore()
+	{
+		return score;
+	}
+
+	public final void setScore(int newScore)
+	{
+		score = newScore;
+	}
+
+}
