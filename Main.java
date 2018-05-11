de28cd72179e697dab491c9747b03597960f3b74
DylanCordoza <dylan.cordoza@gmail.com>
2018-05-11 13:52:52 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
+import java.util.Scanner
 public class Main {
 	public static void main(String []args){
 		System.out.print("Welcome to Go Fish\n\nPlease enter your name:\n");
-		scanner n = new scanner(System.in);
+		Scanner n = new Scanner(System.in);
 		string name = n.readLine();
 		//initiate hand & player here
 
@@ -10,7 +11,7 @@ public class Main {
 			//print player's hand
 			//opponent's turn
 			System.out.print("\nWhat card would you like to ask for?\n");
-			scanner c = new scanner(System.in);
+			Scanner c = new Scanner(System.in);
 			String command = n.readLine();			
 		//}
 	}
