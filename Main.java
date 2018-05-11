e3d6e2bcc5a734d9e42c8faa3c90e6913149e8a8
DylanCordoza <dylan.cordoza@gmail.com>
2018-05-10 23:53:01 -0700

    Main.java uploaded
    Here's the framework for the main part of the code, which will be used to combine the code from the other files.

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String []args){
+		System.out.print("Welcome to Go Fish\n\nPlease enter your name:\n");
+		scanner n = new scanner(System.in);
+		string name = n.readLine();
+		//initiate hand & player here
+
+		//while(deck.size != 0){
+			//print opponent's hand
+			//print player's hand
+			//opponent's turn
+			System.out.print("\nWhat card would you like to ask for?\n");
+			scanner c = new scanner(System.in);
+			String command = n.readLine();			
+		//}
+	}
+
+}
